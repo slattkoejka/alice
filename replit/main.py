6b6008f1c9ae567f017c7e65f8ebc5ad54b3b756
STopkin <stopkin0@gmail.com>
2026-04-01 11:54:33 +0700

    guess a city5

--- replit/main.py
+++ replit/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import logging
 import random
+import os
 
 app = Flask(__name__)
 
@@ -178,4 +179,5 @@ def get_first_name(req):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
