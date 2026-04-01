7d6f798d43cf9923bbc51a1f54eb78dd521902a4
STopkin <stopkin0@gmail.com>
2026-04-01 20:15:55 +0700

    guess a city5

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ cities = {
 
 sessionStorage = {}
 
+last_response = []
+
 @app.route('/')
 def health_check():
     return ''
+
 
 @app.route('/post', methods=['POST'])
 def main():
@@ -31,15 +34,22 @@ def main():
     logging.info(f'Response: {response!r}')
     return jsonify(response)
 
+
 def handle_dialog(res, req):
+    global last_response
     user_id = req['session']['user_id']
 
     if req['session']['new']:
         sessionStorage[user_id] = {'first_name': None}
         res['response']['text'] = 'Привет! Назови свое имя!'
-        res['response']['buttons'] = []  # кнопки пока пустые
+        res['response']['buttons'] = [{'title': 'Помощь', 'hide': False}]  # кнопки пока пустые
         res['response']['end_session'] = False
+        last_response = res
         return
+
+    if req['request']['command'].lower() == 'помощь':
+        res['response']['text'] = 'Это игра "Угадай город". Я загадываю, вы - угадываете!'
+        res['response']['buttons'] = last_response['response']['buttons'][:]
 
     if sessionStorage[user_id]['first_name'] is None:
         first_name = get_first_name(req)
@@ -47,8 +57,10 @@ def handle_dialog(res, req):
             res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
         else:
             sessionStorage[user_id]['first_name'] = first_name
-            res['response']['text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Какой город хочешь увидеть?'
-            res['response']['buttons'] = [{'title': city.title(), 'hide': True} for city in cities]
+            res['response'][
+                'text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Какой город хочешь увидеть?'
+            res['response']['buttons'] =  [{'title': city.title(), 'hide': True} for city in cities] + res['response']['buttons']
+            last_response = res
         return
 
     city = get_city(req)
@@ -62,15 +74,22 @@ def handle_dialog(res, req):
     else:
         res['response']['text'] = 'Первый раз слышу об этом городе. Попробуй еще разок!'
 
+    last_response = res
+
+
 def get_city(req):
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.GEO':
-            return entity['value'].get('city', None)
+    if req['request']['nlu']['entities']:
+        for entity in req['request']['nlu']['entities']:
+            if entity['type'] == 'YANDEX.GEO':
+                return entity['value'].get('city', None)
+
 
 def get_first_name(req):
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.FIO':
-            return entity['value'].get('first_name', None)
+    if req['request']['nlu']['entities']:
+        for entity in req['request']['nlu']['entities']:
+            if entity['type'] == 'YANDEX.FIO':
+                return entity['value'].get('first_name', None)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
