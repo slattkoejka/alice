201087db4a1e515291f4af61e240aa820411e804
STopkin <stopkin0@gmail.com>
2026-04-01 17:08:16 +0700

    guess a city5

--- main.py
+++ main.py
@@ -1,70 +1,94 @@
+# импортируем библиотеки
+import os
 from flask import Flask, request, jsonify
 import logging
-import random
+from waitress import serve
 
 app = Flask(__name__)
+
 logging.basicConfig(level=logging.INFO)
 
-cities = {
-    'москва': ['1540737/daa6e420d33102bf6947', '213044/7df73ae4cc715175059e'],
-    'нью-йорк': ['1652229/728d5c86707054d4745f', '1030494/aca7ed7acefde2606bdc'],
-    'париж': ['1652229/f77136c2364eb90a3ea8', '3450494/aca7ed7acefde22341bdc']
-}
-
 sessionStorage = {}
+
+
+@app.route('/')
+def health_check():
+    return ''
+
 
 @app.route('/post', methods=['POST'])
 def main():
     logging.info(f'Request: {request.json!r}')
+
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
-        'response': {'end_session': False}
+        'response': {
+            'end_session': False
+        }
     }
-    handle_dialog(response, request.json)
-    logging.info(f'Response: {response!r}')
+
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+
     return jsonify(response)
 
-def handle_dialog(res, req):
+
+def handle_dialog(req, res):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
-        sessionStorage[user_id] = {'first_name': None}
-        res['response']['text'] = 'Привет! Назови свое имя!'
-        res['response']['buttons'] = []  # кнопки пока пустые
-        res['response']['end_session'] = False
-        return
-
-    if sessionStorage[user_id]['first_name'] is None:
-        first_name = get_first_name(req)
-        if first_name is None:
-            res['response']['text'] = 'Не расслышала имя. Повтори, пожалуйста!'
-        else:
-            sessionStorage[user_id]['first_name'] = first_name
-            res['response']['text'] = f'Приятно познакомиться, {first_name.title()}. Я Алиса. Какой город хочешь увидеть?'
-            res['response']['buttons'] = [{'title': city.title(), 'hide': True} for city in cities]
-        return
-
-    city = get_city(req)
-    if city in cities:
-        res['response']['card'] = {
-            'type': 'BigImage',
-            'title': 'Этот город я знаю.',
-            'image_id': random.choice(cities[city])
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
         }
-        res['response']['text'] = 'Я угадал!'
-    else:
-        res['response']['text'] = 'Первый раз слышу об этом городе. Попробуй еще разок!'
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
 
-def get_city(req):
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.GEO':
-            return entity['value'].get('city', None)
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо',
+        'Я покупаю',
+        'Я куплю'
+    ]:
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
 
-def get_first_name(req):
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.FIO':
-            return entity['value'].get('first_name', None)
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    serve(app, host='0.0.0.0', port=port)
+    # app.run()
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ffllaapp # Your app working directory!!!
+cd alice # Your app working directory!!!
 export PORT=5000
 unset PIP_USER
 
